bf587b00a1d40b5e02f5bd8d26e03773d85ae418
lizhi02 <lizhi02@rd.netease.com>
2023-08-30 19:28:40 +0800

    init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,45 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
+    "env": {
+      "browser": true,
+      "node": true,
+      "es2021": true
+    },
+    "parser": "vue-eslint-parser",
+    "extends": [
+      "eslint:recommended",
+      "plugin:vue/vue3-recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:prettier/recommended",
+      "eslint-config-prettier"
+    ],
+    "parserOptions": {
+      "ecmaVersion": "latest",
+      "parser": "@typescript-eslint/parser",
+      "sourceType": "module",
+      "ecmaFeatures": {
+        "jsx": true
+      }
+    },
+    "plugins": ["vue", "@typescript-eslint", "prettier"],
+    "rules": {
+      "vue/multi-word-component-names": "off", // 禁用vue文件强制多个单词命名
+      "@typescript-eslint/no-explicit-any": ["off"], //允许使用any
+      "@typescript-eslint/no-this-alias": [
+        "error",
+        {
+          "allowedNames": ["that"] // this可用的局部变量名称
+        }
+      ],
+      "@typescript-eslint/ban-ts-comment": "off", //允许使用@ts-ignore
+      "@typescript-eslint/no-non-null-assertion": "off", //允许使用非空断言
+      "no-console": [
+        //提交时不允许有console.log
+        "warn",
+        {
+          "allow": ["warn", "error"]
+        }
+      ],
+      "no-debugger": "warn" //提交时不允许有debugger
+    }
   }
-}
+  
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,15 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path"; //这个path用到了上面安装的@types/node
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue()],
+  //这里进行配置别名
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": path.resolve("./src"), // @代替src
+      "#": path.resolve("./types"), // #代替types
+    },
+  },
+});
